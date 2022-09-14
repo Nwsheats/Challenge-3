9faa5f5f8a6b5a55e75488cae5ceed39a9d17f6f
Nwsheats <nwsheats@gmail.com>
2022-09-13 23:14:24 -0500

    created vars for characters

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment Code
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+",
+"~", "?", "<", ">"];
+var numericChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const Characters = lowerCase.concat(upperCase, specialChar, numericChar);
+console.log(Characters);
+
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +22,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+console.log(Characters);
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
