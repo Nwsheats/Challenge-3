d4dee1d53f8db26d582a93d69f26347fbf4470cc
Nwsheats <nwsheats@gmail.com>
2022-09-17 10:50:48 -0500

    added js notation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// The generatePassword function has a prompt for the password size, converts the size variable into an integer called SizeInt using the ParseInt function. This is so that if a non-number is
+// entered for prompt, it will return an alert and start the function over. SizeInt is also used the same way to limit the size value between 8 and 128. 
 
 function generatePassword() {
   var password = ""
@@ -14,6 +16,11 @@ function generatePassword() {
     alert("You must choose a number!")
     return generatePassword();
   }
+
+// the variables for the confirm statements are stated as false. I used a 'While Loop' to make sure that while they were all false, the confirm messages would continue to repeat, forcing the user
+// to pick at least one confirm statement. 
+// if they click Cancel on all 4, an alert statement pops up after to tell them to pick one. 
+
   var lchoice = false;
   var uchoice = false;
   var schoice = false;
@@ -27,6 +34,9 @@ function generatePassword() {
       alert("You must pick one type of character!");
     }
   }
+
+// Here is the my 'For Loop' that utilizes the DecisionArray function to return a random series of characters.
+
   const Characters = DecisionArray(lchoice, uchoice, schoice, nchoice);
   for (let i = 0; i < sizeInt; i++) {
     let randPass = Math.random() * Characters.length;
@@ -36,6 +46,9 @@ function generatePassword() {
   }
   return password;
 }
+
+// The DecisionArray function defines a 'totals' array as empty, defines the 4 other arrays for types of characters, then uses an If statment below. If the confirm values are true, the corresponding
+// array is concatenated into the 'totals' array.
 
   function DecisionArray(lchoice, uchoice, schoice, nchoice) {
     let totals = [];
@@ -57,12 +70,10 @@ function generatePassword() {
     return totals;
   }
 
-
 function writePassword() {
   var passwordText = document.querySelector("#password");
   let password = generatePassword()
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
